423d66bcf85cc39927db925ed12892ee93a1ad5d
Muhammad Athar <muhammad.athar@agoda.com>
2020-02-09 11:48:50 +0700

    Chapter 2 - Arrays

---
+++ arrays.js
@@ -0,0 +1,19 @@
+/* Chapter 2 */
+
+/* Arrays */
+
+/* Excercise */
+
+// Reverse an Array
+
+// def reverse(A):
+//  N = len(A)
+//  for i in xrange(N // 2):
+//    k = N - i - 1
+//    A[i], A[k] = A[k], A[i]
+//  return A
+
+let fruits = ["Banana", "Orange", "Apple", "Mango"]
+fruits.reverse()
+
+console.log(fruits)
